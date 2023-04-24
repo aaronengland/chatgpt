c8113d78aa5036e6573547ff756713c011965509
Aaron England <aaron.england.dev@gmail.com>
2023-04-24 09:37:33 -0600

    initial commit

---
+++ chatgpt/functions.py
@@ -0,0 +1,19 @@
+import openai
+
+# class
+class ChatGPT:
+	def __init__(self, str_key):
+		self.str_key
+	def send_prompt(self, str_prompt):
+		# save key
+	    openai.api_key = self.str_key
+	    completion = openai.ChatCompletion.create(
+	      model='gpt-3.5-turbo',
+	      messages=[
+	        {'role': 'user', 'content': str_prompt}
+	      ]
+	    )
+	    # response
+	    str_response = completion.choices[0].message.content
+	    # return
+	    return str_response
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+      name='chatgpt',
+      version='0.0.1',
+      description='Interact with ChatGPT API via Python',
+      url='http://github.com/chatgpt',
+      author='Aaron England',
+      author_email='aaron.england.dev@gmail.com',
+      license='MIT',
+      packages=['chatgpt'],
+      zip_safe=False,
+)
